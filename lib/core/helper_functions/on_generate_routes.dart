fbd86274f7da57cfbfdcc6176ea6091e68e71719
abdelrahman <bad606609@gmail.com>
2025-05-04 20:24:57 +0300

    create on generate routes function

---
+++ lib/core/helper_functions/on_generate_routes.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+/*************  ✨ Windsurf Command ⭐  *************/
+Route<dynamic> onGenerateRoutes(RouteSettings settings) {
+  switch (settings.name) {
+    
+    default:
+      return MaterialPageRoute(
+        builder: (context) => Scaffold(
+          body: Center(
+            child: Text('No route defined for ${settings.name}'),
+          ),
+        ),
+      );
+  }
+}
+/*******  f493da44-e855-4b10-a415-1d8edc8b32a0  *******/  
