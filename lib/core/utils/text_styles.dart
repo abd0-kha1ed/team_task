26b5bde4b0accedd47d4ccbea994c352a8057217
abdelrahman <bad606609@gmail.com>
2025-05-05 12:19:39 +0300

    Add AppTextStyles class for consistent text styling

---
+++ lib/core/utils/text_styles.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const String _fontFamily = 'Poppins';
+
+  static const TextStyle bold20 = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle regular16 = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle medium18 = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle light14 = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 14,
+    fontWeight: FontWeight.w300,
+  );
+
+  static const TextStyle semiBold24 = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle italic16 = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 16,
+    fontStyle: FontStyle.italic,
+  );
+}
