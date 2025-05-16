7c85880f031b449c04440d09325381d6430d94bc
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-05-16 19:17:20 +0300

    Refactor service locator: streamline registration of AddNewTask dependencies and improve code formatting.

--- lib/core/functions/service_locator.dart
+++ lib/core/functions/service_locator.dart
@@ -2,19 +2,17 @@ import 'package:get_it/get_it.dart';
 import 'package:dio/dio.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:team_task/core/api/dio_consumer.dart';
+import 'package:team_task/feature/add_new_task/data/data_source/add_new_task_remote_data_source.dart';
+import 'package:team_task/feature/add_new_task/data/repo/add_new_task_repo_impl.dart';
 import 'package:team_task/feature/auth/data/data_source/remote_data_source.dart';
 import 'package:team_task/feature/auth/data/repo/auth_repo_impl.dart';
 
 final getIt = GetIt.instance;
 
-void setupServiceLocator(SharedPreferences sharedPreferences) async{
-getIt.registerSingleton<SharedPreferences>(sharedPreferences);
+void setupServiceLocator(SharedPreferences sharedPreferences) async {
+  getIt.registerSingleton<SharedPreferences>(sharedPreferences);
 
-  getIt.registerSingleton<DioConsumer>(
-    DioConsumer(
-      Dio(),
-    ),
-  );
+  getIt.registerSingleton<DioConsumer>(DioConsumer(Dio()));
 
   getIt.registerSingleton<RemoteDataSource>(
     RemoteDataSource(api: getIt.get<DioConsumer>()),
@@ -22,5 +20,14 @@ getIt.registerSingleton<SharedPreferences>(sharedPreferences);
 
   getIt.registerSingleton<AuthRepoImpl>(
     AuthRepoImpl(remoteDataSource: getIt.get<RemoteDataSource>()),
+  );
+
+  getIt.registerSingleton<AddNewTaskRemoteDataSource>(
+    AddNewTaskRemoteDataSource(dio: getIt.get<DioConsumer>()),
+  );
+  getIt.registerSingleton<AddNewTaskRepoImpl>(
+    AddNewTaskRepoImpl(
+      addNewTaskRemoteDataSource: getIt.get<AddNewTaskRemoteDataSource>(),
+    ),
   );
 }
