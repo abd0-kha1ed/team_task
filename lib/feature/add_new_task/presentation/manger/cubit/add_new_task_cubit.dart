def4bbddfad77e4fe47955618ced5c6f710a0d1a
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-05-16 19:19:42 +0300

    Implement AddNewTaskCubit with state management for adding tasks

--- lib/feature/add_new_task/presentation/manger/cubit/add_new_task_cubit.dart
+++ lib/feature/add_new_task/presentation/manger/cubit/add_new_task_cubit.dart
@@ -1,9 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:team_task/feature/add_new_task/domain/enitites/task_entity.dart';
+import 'package:team_task/feature/add_new_task/domain/repo/add_new_task_repo.dart';
 
 part 'add_new_task_state.dart';
 
 class AddNewTaskCubit extends Cubit<AddNewTaskState> {
-  AddNewTaskCubit() : super(AddNewTaskInitial());
+  final AddNewTaskRepo addNewTaskRepo;
+  AddNewTaskCubit({required this.addNewTaskRepo}) : super(AddNewTaskInitial());
+
+  Future<void> addNewTask(TaskEntity taskEntity) async {
+    emit(AddNewTaskLoading());
+    var result = await addNewTaskRepo.addNewTask(taskEntity);
+    result.fold(
+      (error) => emit(AddNewTaskError(error: error.toString())),
+      (taskEntity) => emit(AddNewTaskSuccess(taskEntity: taskEntity)),
+    );
+  }
 }
